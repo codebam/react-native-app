e47d62d26dd8854fd26d3321419ce17bf4c7bd72
makano <makanobush@gmail.com>
2023-10-20 23:33:01 +0300

    ugh

--- src/data/messages.js
+++ src/data/messages.js
@@ -1,62 +1,38 @@
 import { useState } from "react";
 
+
 export default class Messages {
-  /**
-   * Create a new Messages instance.
-   */
-  constructor() {
-    const [messages, setMessages] = useState([]);
-    this.messages.set = setMessages;
-    this.messages.all = messages;
-  }
+	messages = {};
 
-  /**
-   * Set the messages array.
-   * @param {MessageData[]} messages - An array of messages.
-   * @returns {Messages} The updated Messages instance.
-   */
-  set(messages) {
-    this.messages.set(messages);
-    return this;
-  }
+	constructor(){
+		const [messages, setMessages] = useState([]);
+		this.messages.set = setMessages;
+		this.messages.all = messages;
+	}
 
-  /**
-   * Get all messages.
-   * @returns {MessageData[]} An array of all messages.
-   */
-  all() {
-    return this.messages.all;
-  }
+	set(messages){
+		this.messages.set(messages);
+		return this;
+	}
 
-  /**
-   * Add a new message to the array.
-   * @param {MessageData} message - The message to add.
-   * @returns {Messages} The updated Messages instance.
-   */
-  add(message) {
-    this.messages.set([...this.messages.all, message]);
-    return this;
-  }
+	all(){
+		return this.messages.all;
+	}
 
-  /**
-   * Set a message at a specific index in the array.
-   * @param {number} index - The index at which to set the message.
-   * @param {MessageData} options - The new message data.
-   * @returns {Messages} The updated Messages instance.
-   */
-  setMessage(index, options) {
-    let messages = [...this.messages.all];
-    messages[index] = options;
-    this.messages.set(messages);
-    return this;
-  }
+	add(message){
+		this.messages.set([...this.messages.all, message]);
+		return this;
+	}
 
-  /**
-   * Get a message at a specific index.
-   * @param {number} index - The index of the message to retrieve.
-   * @returns {MessageData} The message at the specified index.
-   */
-  at(index) {
-    return this.messages.all.at(index);
-  }
+
+	setMessage(index, options){
+		let messages = [...this.messages.all];
+		messages[index] = options;
+		this.messages.set(messages);
+		return this;
+	}
+
+	at(index){
+		return this.messages.all.at(index);
+	}
 }
