58545fbea187bc9d3f8f9101e0b735d4050fa33e
Sean Behan <codebam@riseup.net>
2023-10-20 13:04:59 -0400

    add basic input

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [textInput, setTextInput] = React.useState("");
+	return (
+		<View style={styles.container}>
+			<Text>Input text below</Text>
+			<TextInput
+				value={textInput}
+				onChange={setTextInput}
+				style={styles.input}
+			/>
+			<Button title="submit" color="#000" />
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	input: {
+		backgroundColor: "#eee",
+	},
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
